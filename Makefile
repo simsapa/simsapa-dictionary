0edf9e7bc7662e896d05c7c64804e8a6ebb0d86e
Gambhiro <gambhiro.bhikkhu.85@gmail.com>
2019-09-11 18:53:11 +0100

    first

---
+++ Makefile
@@ -0,0 +1,5 @@
+all:
+	@echo "There is no default target."
+
+sources_to_markdown:
+	./scripts/sources_to_markdown.sh
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+
+pub mod models;
