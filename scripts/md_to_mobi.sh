31e1d4693b3bda0e2d75facd04a9985ddf1a155d
Gambhiro <gambhiro.bhikkhu.85@gmail.com>
2019-09-19 18:42:17 +0100

    scripts

--- scripts/md_to_mobi.sh
+++ scripts/md_to_mobi.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 cargo run -- markdown_to_mobi \
-	--markdown_paths_list ./scripts/two_ncped.txt \
-	--mobi_path ../simsapa-dictionary-data/ncped.mobi \
+	--markdown_paths_list ./scripts/dict_md_paths.txt \
+	--mobi_path ../simsapa-dictionary-data/dictionary.mobi \
 	--mobi_compression 0 \
 	--dont_remove_generated_files \
 	--kindlegen_path $HOME/lib/kindlegen/kindlegen
